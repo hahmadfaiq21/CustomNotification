944a58d1b606da8c2a5c2c1797adb87d66fc0e42
Husain Ahmad Faiq <hahmadfaiq@gmail.com>
2024-10-15 10:19:22 +0700

    Adding Intent with data in MainActivity to DetailActivity

--- app/src/main/java/com/github/hahmadfaiq21/customnotification/MainActivity.kt
+++ app/src/main/java/com/github/hahmadfaiq21/customnotification/MainActivity.kt
@@ -9,6 +9,8 @@ import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.app.TaskStackBuilder
+import androidx.core.widget.addTextChangedListener
 import com.github.hahmadfaiq21.customnotification.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +21,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnOpenDetail.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val message = binding.etMessage.text.toString()
+            if (title.isEmpty()) {
+                binding.etTitle.error = "Title cannot be empty"
+            } else if (message.isEmpty()) {
+                binding.etMessage.error = "Message cannot be empty"
+            } else {
+                val intent = Intent(this, DetailActivity::class.java)
+                intent.putExtra(DetailActivity.EXTRA_TITLE, title)
+                intent.putExtra(DetailActivity.EXTRA_MESSAGE, message)
+                startActivity(intent)
+            }
+        }
+        binding.btnSendNotification.isEnabled = false
+        binding.etTitle.addTextChangedListener { checkInputFilled() }
+        binding.etMessage.addTextChangedListener { checkInputFilled() }
         binding.btnSendNotification.setOnClickListener {
             sendNotification()
+        }
+    }
+
+    private fun checkInputFilled() {
+        binding.apply {
+            btnSendNotification.isEnabled =
+                etTitle.text.toString().isNotBlank() && etMessage.text.toString().isNotBlank()
         }
     }
 
@@ -29,13 +56,16 @@ class MainActivity : AppCompatActivity() {
         val message = binding.etMessage.text.toString()
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val intent = Intent(this, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
+        val notificationDetailIntent = Intent(this, DetailActivity::class.java)
+        notificationDetailIntent.putExtra(DetailActivity.EXTRA_TITLE, title)
+        notificationDetailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, message)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(notificationDetailIntent)
+            getPendingIntent(
+                NOTIFICATION_ID,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(title)
